f18e38b9c9db8539a9ac94cbfcf5ee98eee72234
Darell Mona <darell.mona242@gmail.com>
2022-06-26 21:41:03 +0800

    add my-app to repository

---
+++ basic reactJS/my-app/src/components/Hello.js
@@ -0,0 +1,12 @@
+const Hello = ({name, age}) =>
+{
+  //const name = "Darell Mona";
+  return (
+    <>
+      <h1>Hello World. This is {name} {age} years old</h1>
+      <p>Yuhu is here</p>
+    </>
+  );
+}
+
+export default Hello
